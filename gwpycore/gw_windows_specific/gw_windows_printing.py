292b278b4df0b60e36766f4e3afefefff37318d6
Craig Jones <craig@k6nnl.com>
2020-08-25 21:28:06 -0700

    Added a fill_in_pdf function based on PyPDF4.

--- gwpycore/gw_windows_specific/gw_windows_printing.py
+++ gwpycore/gw_windows_specific/gw_windows_printing.py
@@ -1,8 +1,22 @@
+from pypdf import PdfFileWriter
 import win32api, win32print
 
-def printPdf(pdfName: str, printer="default"):
+def print_pdf(pdfName: str, printer="default"):
     if printer == "default":
         printer = win32print.GetDefaultPrinter()
     win32api.ShellExecute(0, "print", pdfName, '/d:"%s"' % printer, ".", 0)
 
-__all__ = ("printPdf",)
+
+def fill_in_pdf(template_pdf, field_values, filename):
+    output = PdfFileWriter(filename)
+    output.have_viewer_render_fields()
+    for page_no in range(template_pdf.numPages):
+        template_page = template_pdf.getPage(page_no)
+        output.addPage(template_page)
+        page = output.getPage(page_no)
+        output.updatePageFormFieldValues(page, field_values, read_only=True)
+    output.write()
+    print("Created '%s'" % (filename))
+
+
+__all__ = ("print_pdf", "fill_in_pdf")
