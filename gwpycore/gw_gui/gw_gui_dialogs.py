3fb98eca0902c38b6b81c1ef9cf907ccfc732e58
Craig Jones <craig@k6nnl.com>
2020-08-30 00:15:24 -0700

    Added a new dialog box with a combo box: ask_user_to_choose()

--- gwpycore/gw_gui/gw_gui_dialogs.py
+++ gwpycore/gw_gui/gw_gui_dialogs.py
@@ -1,8 +1,10 @@
 """
 Frequently Used Message Dialog Boxes
 """
+from typing import List
 from PyQt5.QtCore import QCoreApplication, QObject, Qt, QTimer
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QMessageBox, QPushButton, QLabel, QComboBox, QVBoxLayout
 
 ICON_ERROR = QMessageBox.Critical
 ICON_WARN = QMessageBox.Warning
@@ -35,5 +37,50 @@ def ask_user_to_confirm(question: str, icon: QMessageBox.Icon = ICON_QUESTION, p
     box.raise_()
     return box.exec_() == QMessageBox.Yes
 
+class ChoicesDialog(QDialog):
+    def __init__(self, parent=None):
+        super(ChoicesDialog, self).__init__(parent)
+        self.setModal(True)
+        self.question = QLabel("Please choose:")
+        self.choices = QComboBox()
+        self.cancel_button = QPushButton("Cancel")
+        self.select_button = QPushButton("Select")
+        # Make the user specifically click the Select button. Hitting Enter might be too easy and set the wrong choice.
+        self.select_button.setDefault(False)
+        # https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
+        self.icon_label = QLabel()
+        # TODO https://openapplibrary.org/dev-tutorials/qt-icon-themes
+        self.icon_label.setWindowIcon(QIcon.fromTheme("dialog-question"))
+        layout = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout2.addWidget(self.question)
+        layout2.addWidget(self.choices)
+        layout2.addWidget(self.select_button)
+        layout.addWidget(self.icon_label)
+        layout.addLayout(layout2)
+        self.setLayout(layout)
+        self.cancel_button.clicked.connect(self.reject)
+        self.select_button.clicked.connect(self.make_selection)
+        self.setStyleSheet(
+            """
+            QPushButton {font-size:14pt;}
+            QComboBox {font-size:14pt;}
+            QLabel {font-size:14pt;}
+        """)
 
-__all__ = ("inform_user_about_issue", "ask_user_to_confirm", "ICON_ERROR", "ICON_WARN", "ICON_INFO", "ICON_QUESTION", "STD_DIALOG_OPTS")
+    def make_selection(self):
+        # 0 = rejected (Escape), so a positive result needs to be 1-based
+        self.done(self.choices.currentIndex()+1)
+
+def ask_user_to_choose(question: str, choices: List[str], icon: QMessageBox.Icon = ICON_QUESTION, parent: QObject = None, title="Please Make a Selection") -> int:
+    box = ChoicesDialog(parent)
+    box.setWindowTitle(title)
+    box.question.setText(question)
+    box.choices.addItems(choices)
+    box.show()
+    QCoreApplication.processEvents()
+    box.raise_()
+    return box.exec_()
+
+
+__all__ = ("inform_user_about_issue", "ask_user_to_confirm", "ask_user_to_choose", "ICON_ERROR", "ICON_WARN", "ICON_INFO", "ICON_QUESTION", "STD_DIALOG_OPTS")
