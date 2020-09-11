5f0118c4bc11a37ee716a6e933032e5dca81a32e
Craig Jones <craig@k6nnl.com>
2020-09-11 15:11:55 -0700

    Pushed inspect_config into CoreActions Changed CoreActions to look in self.config for info instead (of self).

--- gwpycore/gw_gui/gw_gui_core_actions.py
+++ gwpycore/gw_gui/gw_gui_core_actions.py
@@ -4,17 +4,19 @@ from gwpycore import ICON_WARNING, ask_user_to_confirm, inform_user
 
 class CoreActions:
     """
-    This super class that can be a (third) class from which a QDialog can 
-    inherit. It provides default handlers for a half dozen functions 
-    (About, Help, etc.)
+    This super class that can be a (third) class from which a QDialog can inherit.
+    It connects default handlers to any of the following actions that exist:
+        action_About
+        action_Report_Bug
+        action_Exit
+        action_Help
+        action_Updates
+        action_Inspect_Config
+        action_Distraction_Free
     """
+
     def __init__(self) -> None:
-        self.confirm_exit = True
-        self.documentation_url = ""
-        self.report_bug_url = ""
-        self.latest_release_url = ""
-        self.application_title = ""
-        self.version = "Unknown"
+        pass
 
     def not_implemented(self):
         inform_user(
@@ -25,26 +27,24 @@ class CoreActions:
         )
 
     def about(self):
-        inform_user(
-            f"{self.application_title}\nVersion: {self.version}",
-            parent=self,
-            title="About",
-        )
+        if hasattr(self.config, "application_title"):
+            info = f"{self.config.application_title}\nVersion: {self.config.version}"
+            inform_user(info, parent=self, title="About")
 
     def report_bug(self):
-        if self.report_bug_url:
-            webbrowser.open(self.report_bug_url, new=2)
+        if hasattr(self.config, "report_bug_url"):
+            webbrowser.open(self.config.report_bug_url, new=2)
         else:
             self.not_implemented()
 
     def home_page(self):
-        if self.documentation_url:
-            webbrowser.open(self.documentation_url, new=2)
+        if hasattr(self.config, "documentation_url"):
+            webbrowser.open(self.config.documentation_url, new=2)
         else:
             self.not_implemented()
 
     def check_for_updates(self):
-        if self.latest_release_url:
+        if hasattr(self.config, "latest_release_url"):
             # FIXME Scrape the version number from self.latest_release_url
             self.not_implemented()
         else:
@@ -55,18 +55,34 @@ class CoreActions:
         self.not_implemented()
 
     def close_application(self):
-        if not self.confirm_exit:
+        if not self.config.confirm_exit:
             self.close()
         if ask_user_to_confirm("Exit, are you sure?", parent=self):
             self.close()
+        # FIXME closes anyway
+
+    def inspect_config(self):
+        info = []
+        for key, value in vars(self.config).items():
+            info.append(f"{key} \t= {value.__repr__()}")
+        info.sort()
+        inform_user("\n".join(info), parent=self, title="Diagnostic: Configuration Settings")
 
     def connect_core_actions(self):
-        self.action_About.triggered.connect(self.about)
-        self.action_Bug.triggered.connect(self.report_bug)
-        self.action_Exit.triggered.connect(self.close_application)
-        self.action_Help.triggered.connect(self.home_page)
-        self.action_Updates.triggered.connect(self.check_for_updates)
-        self.action_Distraction_Free.triggered.connect(self.full_screen)
+        if hasattr(self, "action_About"):
+            self.action_About.triggered.connect(self.about)
+        if hasattr(self, "action_Report_Bug"):
+            self.action_Report_Bug.triggered.connect(self.report_bug)
+        if hasattr(self, "action_Exit"):
+            self.action_Exit.triggered.connect(self.close_application)
+        if hasattr(self, "action_Help"):
+            self.action_Help.triggered.connect(self.home_page)
+        if hasattr(self, "action_Updates"):
+            self.action_Updates.triggered.connect(self.check_for_updates)
+        if hasattr(self, "action_Inspect_Config"):
+            self.action_Inspect_Config.triggered.connect(self.inspect_config)
+        if hasattr(self, "action_Distraction_Free"):
+            self.action_Distraction_Free.triggered.connect(self.full_screen)
 
 
 __all__ = ("CoreActions",)
