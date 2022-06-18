e1b5c66d841e87f618d5705a1ce41362e5199326
Craig Jones <craig@k6nnl.com>
2022-06-18 14:49:14 -0700

    Corrected use of strptime

--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -1,10 +1,8 @@
-import time
-
+from datetime import datetime
 from gwpycore import timestamp
 
-
 def test_timestamp():
-    currentTime = time.strptime("30 Jun 20 13:59:59", "%d %b %y %H:%M:%S")
+    currentTime = datetime.strptime("30 Jun 20 13:59:59", "%d %b %y %H:%M:%S")
     assert timestamp(currentTime) == "2020_06_30_135959"
     assert timestamp(currentTime, format="%Y%m%d%H%M%S") == "20200630135959"
     assert timestamp(currentTime, format="%Y-%m-%d-%H%M%S") == "2020-06-30-135959"
