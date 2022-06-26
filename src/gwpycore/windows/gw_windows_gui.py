192a0d0888af968405e56a182c9374f97f096938
Craig Jones <craig@k6nnl.com>
2022-06-26 03:00:48 -0700

    Added a set_windows_wallpaper() function. (No unit tests yet.)

---
+++ src/gwpycore/windows/gw_windows_gui.py
@@ -0,0 +1,6 @@
+import ctypes
+
+def set_windows_wallpaper(filepath):
+    SPI_SETDESKWALLPAPER = 20
+    image_path = str(filepath).strip()
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 0)
