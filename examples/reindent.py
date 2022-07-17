b1b59354a4542312377d557b7a09ed6944eec249
Craig Jones <craig@k6nnl.com>
2022-07-16 17:06:54 -0700

    Added comments to the reindent.py example.

--- examples/reindent.py
+++ examples/reindent.py
@@ -12,22 +12,22 @@
 -v (--verbose)  Verbose.   Print informative msgs; else no output.
 -h (--help)     Help.      Print this usage information and exit.
 
-Change Python (.py) files to use 4-space indents and no hard tab characters.
-Also trim excess spaces and tabs from ends of lines, and remove empty lines
+Changes Python (.py) files to use 4-space indents and no hard tab characters.
+Also trim excess spaces and tabs from ends of lines, and removes empty lines
 at the end of files. Also ensure the last line ends with a newline.
 
-If no paths are given on the command line, reindent operates as a filter,
+If no paths are given on the command line, `reindent` operates as a filter,
 reading a single source file from standard input and writing the transformed
 source to standard output.  In this case, the -d, -r and -v flags are
 ignored.
 
-You can pass one or more file and/or directory paths.  When a directory
-path, all .py files within the directory will be examined, and, if the -r
-option is given, likewise recursively for subdirectories.
+You can pass in one or more file and/or directory paths. When a directory
+path is named, all .py files within that directory will be examined, and if
+the -r option is given, likewise recursively for subdirectories.
 
-If output is not to standard output, reindent overwrites files in place,
+If output is not to standard output, `reindent` overwrites files in place,
 renaming the originals with a .bak extension.  If it finds nothing to
-change, the file is left alone.  If reindent does change a file, the changed
+change, the file is left alone. If reindent does change a file, the changed
 file is a fixed-point for future runs (i.e., running reindent on the
 resulting .py file won't change it again).
 
@@ -56,20 +56,63 @@ from typing import Optional
 from gwpycore import (basic_cli_parser, leading_spaces_count, rstrip_special,
                       setup_logging)
 
-CONFIG: argparse.Namespace
-LOG: Logger
+CONFIG: argparse.Namespace = None
+LOG: Logger = None
 
 
 def load_command_line():
-    p: ArgumentParser = basic_cli_parser(__version__, filenames="*", verbose=True, recurse=True, logfile=True)
-    p.add_argument("-d", "--dryrun", dest="dryrun", help="Analyze, but don't make any changes to, files.", action="store_true", default=False)
-    p.add_argument("-n", "--nobackup", dest="makebackup", help='Does not make a ".bak" file before reindenting.', action="store_false", default=True)
+    """
+    This establishes a global `CONFIG` variable with an instance of
+    `argparse.Namespace`, which contains any configuration options that
+    are passed in via the command line.
+
+    By calling `gwpycore.basic_cli_parser()` here, we get a parser with
+    several common arghuments defined. Then, we just have to define any
+    command-line arguments that are speciifc this is particular app
+    (using standard ArgumentParser methods).
+    """
+    arg_parser: ArgumentParser = basic_cli_parser(
+        __version__,
+        filenames="*",
+        recurse=True,
+        logfile=True,
+        verbose=True,   # default (included for example completeness)
+        very_verbose=True,  # default (included for example completeness)
+        nocolor=True,   # default (included for example completeness)
+        command=False,   # default (included for example completeness)
+        devel=False,   # default (included for example completeness)
+        trace=False,   # default (included for example completeness)
+        configfile=False,  # default (included for example completeness)
+        configfile_default="",   # default (included for example completeness)
+        logfile_default="",   # default (included for example completeness)
+        infile=False,  # default (included for example completeness)
+        outfile=False,  # default (included for example completeness)
+    )
+    # We now have argument parsing for: -r (--recurse), -v (--verbose),
+    # -vv (--very-verbose), --logfile, -h (--help), and --version.
+    # They'll be accessed as: CONFIG.recurse (True/False), CONFIG.loglevel
+    # (INFO for -v, DEBUG for -vv), and CONFIG.logfile (str).
+    # (The --help and --version switches are acted upon immediately.)
+
+    # We'll next add a -d (--dryrun) switch, which we'll access via
+    # CONFIG.dryrun (True/False).
+    arg_parser.add_argument("-d", "--dryrun", dest="dryrun", help="Analyze, but don't make any changes to, files.", action="store_true", default=False)
+
+    # And we'll add a -n (--nobackup) switch, which we'll access via
+    # CONFIG.makebackup (True/False).
+    arg_parser.add_argument("-n", "--nobackup", dest="makebackup", help='Does not make a ".bak" file before reindenting.', action="store_false", default=True)
+
     global CONFIG
-    CONFIG = p.parse_args()
+    CONFIG = arg_parser.parse_args()
 
 
 def main():
     load_command_line()
+    # At this point, we have a CONFIG object that contains the values passed
+    # in via the command line. If we wanted to, we could add to or modify those
+    # settings by reading in a config file using ConfigParser (for INI files)
+    # with the help of
+
 
     logfilepath: Optional[Path] = Path(CONFIG.logfile) if CONFIG.logfile else None
     global LOG
