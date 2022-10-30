555039f720cea1413df3e932ca39e77841279897
Craig Jones <craig@k6nnl.com>
2022-10-30 00:27:49 -0700

    Reworked how a ReportLab doc template is created (including enhancements for logical pages within a physical page)

--- src/gwpycore/reportlab/templates/multi_column.py
+++ src/gwpycore/reportlab/templates/multi_column.py
@@ -1,104 +1,74 @@
 import logging
+from pathlib import Path
+from typing import Union
 
 from reportlab.platypus import BaseDocTemplate, PageTemplate, Frame
-from reportlab.lib.pagesizes import LETTER
+
+from .page_sizes import complete_page_spec
 
 LOG = logging.getLogger("main")
 
 __all__ = [
-    "create_col_frames",
-    "MultiColumnDoc",
-    "SheetLabelsDoc",
+    "doc_template_for_page_spec",
 ]
 
 
-def create_col_frames(doc_template: BaseDocTemplate, page_template_id="main_page"):
+def doc_template_for_page_spec(filename: Union[Path, str], page_spec, outline_frames=False):
+    """
+    Instantiates a doc template that contains one page template which contains a
+    frame for each column, or logical page body, or combination thereof.
+
+    :param filename: The file name for the output.
+
+    :param page_spec: A Dictionary with the (logical) page and column specifications.
+
+    :param outline_frames: Whether or not to draw a border aound each frame (for
+    debugging), defaults to False.
+    """
+    doc = BaseDocTemplate(str(filename))
+    complete_page_spec(page_spec)
+    doc.pagesize = page_spec["physical_page_size"]
+    doc.bottomMargin = page_spec["bottom_margin"]
+    doc.topMargin = page_spec["top_margin"]
+    doc.leftMargin = page_spec["left_margin"]
+    doc.rightMargin = page_spec["right_margin"]
+    create_col_frames(doc, page_spec, outline_frames=outline_frames)
+    return doc
+
+
+def create_col_frames(doc_template: BaseDocTemplate, page_spec, outline_frames=False):
     """
     For the given doc template, this establishes a page template containing a
-    frame for each column.
+    frame for each column, or logical page body, or combination thereof.
+
+    :param doc_template: The BaseDocTemplate instance to modify.
+
+    :param page_spec: A Dictionary with the (logical) page and column specifications.
+
+    :param outline_frames: Whether or not to draw a border aound each frame (for
+    debugging), defaults to False.
     """
     frames = []
-    for col_number in range(doc_template.cols):
-        offset = col_number * (doc_template.column_width + doc_template.gutter_width)
-        f = Frame(id=f'col{str(col_number)}',
-                  x1=doc_template.leftMargin + offset,
-                  y1=doc_template.bottomMargin,
-                  width=doc_template.column_width, height=doc_template.column_height,
-                  leftPadding=doc_template.horizontal_padding, rightPadding=doc_template.horizontal_padding,
-                  topPadding=0, bottomPadding=0,
-                  showBoundary=doc_template.outline_frames)
-        frames.append(f)
-    doc_template.addPageTemplates(PageTemplate(id=page_template_id, frames=frames))
 
+    for page_number in range(page_spec["logical_page_count"]):
+        logical_page_width = page_spec["logical_page_size"][0]
+        logical_page_height = page_spec["logical_page_size"][1]
+        page_offset = page_number * logical_page_width
+        col_count = page_spec["column_count"]
+        col_width = (logical_page_width - (col_count - 1) * page_spec["column_gutter"]
+                     - page_spec["binding_margin"] - page_spec["left_margin"] - page_spec["right_margin"]) / col_count
+        col_height = logical_page_height - page_spec["top_margin"] - page_spec["bottom_margin"]
 
-# ############################################################################
-#                                                        MULTI-COLUMN DOCUMENT
-# ############################################################################
-
-class MultiColumnDoc(BaseDocTemplate):
-    """
-    A ReportLab document template with multiple columns (frames) of equal size.
-    """
-    DEFAULT_GUTTER = 18  # 1/4"
-
-    def __init__(self, filename, pagesize=LETTER,
-                 cols=2,
-                 gutter_width=DEFAULT_GUTTER,
-                 outline_frames=0, **kw):
-        super().__init__(filename, pagesize=pagesize, pageTemplates=[], showBoundary=0, **kw)
-        self.horizontal_padding = 0
-        self.cols = cols
-        self.gutter_width = gutter_width
-        self.outline_frames = outline_frames
-        self.calculate_frame_info()
-        create_col_frames(self)
-
-    def calculate_frame_info(self):
-        """
-        The column width and height are automatically determined.
-        """
-        page_width, page_height = self.pagesize
-        self.body_width = page_width - self.leftMargin - self.rightMargin
-        self.column_width = (self.body_width - (self.gutter_width * (self.cols - 1))) / self.cols
-        self.column_height = page_height - self.topMargin - self.bottomMargin
-
-
-# ############################################################################
-#                                                        SHEET LABELS DOCUMENT
-# ############################################################################
-
-class SheetLabelsDoc(BaseDocTemplate):
-    """
-    A ReportLab document template for printing sheets of labels.
-    Given the size of each individual label, the grid size and subsequent
-    margins are automatically determined.
-    """
-    DEFAULT_GUTTER = 0
-    AVERY_5160_WIDTH = 189  # 2 5/8"
-    AVERY_5160_HEIGHT = 72  # 1"
-
-    def __init__(self, filename, pagesize=LETTER,
-                 label_width=AVERY_5160_WIDTH, label_height=AVERY_5160_HEIGHT,
-                 horizontal_padding=0,
-                 outline_frames=0, **kw):
-        super().__init__(filename, pagesize=pagesize, pageTemplates=[], showBoundary=0, **kw)
-        self.horizontal_padding = horizontal_padding
-        self.label_height = label_height
-        self.column_width = label_width
-        self.gutter_width = 0
-        self.outline_frames = outline_frames
-        self.calculate_frame_info()
-        create_col_frames(self)
-
-    def calculate_frame_info(self):
-        """
-        Given the size of each individual label, the grid size and subsequent
-        margins are automatically determined.
-        """
-        page_width, page_height = self.pagesize
-        self.rows = int(page_height / self.label_height)
-        self.cols = int(page_width / self.column_width)
-        self.leftMargin = self.rightMargin = (page_width - (self.column_width * self.cols)) / 2.0
-        self.topMargin = self.bottomMargin = (page_height - (self.label_height * self.rows)) / 2.0
-        self.column_height = self.label_height * self.rows
+        for col_number in range(col_count):
+            col_offset = page_spec["left_margin"] + col_number * (col_width + page_spec["column_gutter"])
+            f = Frame(id=f'page{str(page_number)}_col{str(col_number)}',
+                      x1=page_offset + col_offset,
+                      y1=page_spec["bottom_margin"],
+                      width=col_width, height=col_height,
+                      leftPadding=page_spec["horizontal_padding"],
+                      rightPadding=page_spec["horizontal_padding"],
+                      topPadding=0, bottomPadding=0,
+                      showBoundary=outline_frames)
+            frames.append(f)
+    doc_template.addPageTemplates(PageTemplate(id="physical_page", frames=frames))
 
