e87167a2f1121a39647819263630f58a21595d4e
Craig Jones <craig@k6nnl.com>
2023-02-04 11:46:01 -0800

    Added machine_id()

--- src/gwpycore/core/environ.py
+++ src/gwpycore/core/environ.py
@@ -1,11 +1,16 @@
-import sys
+import sys, os
 from importlib.util import find_spec
 from sys import platform
 
 __all__ = [
+    "machine_id",
     "is_windows",
     "is_module_installed"
 ]
+
+
+def machine_id():
+	return os.environ["COMPUTERNAME"]
 
 
 def is_windows() -> bool:
@@ -16,3 +21,4 @@ def is_module_installed(module_name: str) -> bool:
     if module_name in sys.modules:
         return True
     return find_spec(module_name) is not None
+
